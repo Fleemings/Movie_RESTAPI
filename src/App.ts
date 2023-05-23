5d3dc1c8cfc3ed7f421a7e35a828446e44407227
Milena Fleming <milenasapucaia29@gmail.com>
2023-05-23 18:37:44 +0100

    Updated App file
    Added all the middlewares and necessary imports for the routes

--- src/App.ts
+++ src/App.ts
@@ -1,10 +1,24 @@
+require('dotenv').config();
 import express  from "express";
-import { Config } from "winston/lib/winston/config";
+import config from 'config';
+const cors = require('cors')
+import morganMiddleware from "./Middleware/MorganMiddleware";
+import {movieRoutes} from "./Router/Router";
+import db from '../config/db'
+import Logger from "../config/logger";
+
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+app.use(morganMiddleware)
+app.use('/api', movieRoutes)
 
-app.listen(3000, async () => {
-    console.log('Application is running at port: 3000')
+
+const port = config.get<number>('port')
+
+app.listen(port, async () => {
+    await db()
+    Logger.info(`Application is running at port: ${port}`)
 })
